cc8e4d0ad4fdf6d04dcd9f66dad3de1d98d73c2c
Nello Casolla <nellocasolla446@gmail.com>
2023-03-31 17:36:46 +0200

    final commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import logo from "./logo.svg";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import MyNav from "./components/MyNav";
+import Main from "./components/Main";
+import MyFooter from "./components/MyFooter";
+
+function App() {
+  return (
+    <>
+      <MyNav />
+      <Main />
+      <MyFooter />
+    </>
+  );
+}
+
+export default App;
